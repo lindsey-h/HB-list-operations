d10272e0ae0aac2c8756c25595d8c615babac1f1
Lindsey Hight <lindseydhight@gmail.com>
2021-07-15 20:01:49 +0000

    Five Failures left

--- list_operations.py
+++ list_operations.py
@@ -9,8 +9,7 @@ def head(input_list):
       >>> head(['Jan', 'Feb', 'Mar'])
       'Jan'
     """
-
-    return None
+    return input_list[0]
 
 
 def tail(input_list):
@@ -23,7 +22,7 @@ def tail(input_list):
 
     """
 
-    return []
+    return input_list[1:]
 
 
 def last(input_list):
@@ -36,7 +35,7 @@ def last(input_list):
 
     """
 
-    return []
+    return input_list[2]
 
 
 def top(input_list):
@@ -48,8 +47,7 @@ def top(input_list):
     ['Jan', 'Feb']
 
     """
-
-    return []
+    return input_list[0:-1]
 
 
 def first_three(input_list):
@@ -62,7 +60,7 @@ def first_three(input_list):
 
     """
 
-    return []
+    return input_list[0:3]
 
 
 def last_five(input_list):
@@ -74,8 +72,7 @@ def last_five(input_list):
     [15, 18, 21, 24, 27]
 
     """
-
-    return []
+    return input_list[-5:]
 
 
 def middle(input_list):
@@ -88,7 +85,7 @@ def middle(input_list):
 
     """
 
-    return []
+    return input_list[2:-2]
 
 
 def inner_four(input_list):
@@ -101,7 +98,7 @@ def inner_four(input_list):
 
     """
 
-    return []
+    return input_list[2:6]
 
 
 def inner_four_end(input_list):
@@ -117,7 +114,7 @@ def inner_four_end(input_list):
 
     """
 
-    return []
+    return input_list[-6:-2]
 
 
 def replace_head(input_list):
